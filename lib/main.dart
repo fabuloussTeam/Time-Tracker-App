8549ef239ccc0293c1dc6a8ccda7ad874671005c
fabuloussTeam <tjomemmanuel@gmail.com>
2020-09-15 15:52:11 +0100

    Firebase Authentificate: Initialisation de Application avec firebase_core

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,38 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:timetrackerapp/app/sign_in/sign_in_page.dart';
 
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // create the initialization Future outside of 'build'
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: SignInPage(),
+    return FutureBuilder(
+      future: _initialization,
+        builder: (context, snapshot){
+          if(snapshot.hasError){
+            return Text("Something Went Wrong");
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+                visualDensity: VisualDensity.adaptivePlatformDensity,
+              ),
+              home: SignInPage(),
+            );
+          }
+
+          return Text("Loanding App...");
+        },
     );
   }
 }
