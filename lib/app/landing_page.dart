31a29f7dda53a12b14ccc65f0f496bbc6c909010
fabuloussTeam <tjomemmanuel@gmail.com>
2020-09-17 14:53:15 +0100

    Full Authentification build method Flow Statemanagement Dependency Injection: Ajout de la functionnalyté signIn / signOut

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:timetrackerapp/app/home_page.dart';
 import 'package:timetrackerapp/app/sign_in/sign_in_page.dart';
 
 class LandingPage extends StatefulWidget {
@@ -26,10 +27,8 @@ class _LandingPageState extends State<LandingPage> {
       );
     }
 
-    return Container(
-      child: Center(
-        child: Text("Home page"),
-      ),
+    return HomePage(
+      onSignOut: () => _updateUser(null),
     );
   }
 }
