cbae389950fb693fca26ea4c53b68b358e661730
fabuloussTeam <tjomemmanuel@gmail.com>
2020-09-18 15:18:10 +0100

    Google & Facebook Sign-In: activation du service google sur firebase, creation de la function de connection et test avec succes sur Android

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -23,6 +23,14 @@ class SignInPage extends StatelessWidget {
     }
   }
 
+  Future<void> _signInWithGoogle() async {
+    try {
+      await auth.signInWithGoogle();
+    } catch (e) {
+      print("${e.toString()}");
+    }
+  }
+
  @override
  Widget build(BuildContext context) {
 
@@ -47,7 +55,7 @@ class SignInPage extends StatelessWidget {
               text: "Sign in with Google",
               textColor: Colors.black87,
               color: Colors.white,
-              onPress: (){},
+              onPress: _signInWithGoogle,
             ),
             SizedBox(height: 8.0,),
             SocialSignInButton(
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 // Model User
 class UserModel {
@@ -12,6 +14,7 @@ abstract class AuthBase {
   Stream<UserModel> get onAuthStateChange;
   Future<UserModel> currentUser();
   Future<UserModel> signInAnonymously();
+  Future<UserModel> signInWithGoogle();
   Future<void> signOut();
 }
 
@@ -41,11 +44,41 @@ class Auth implements AuthBase {
     return _userFromFirebase(currentUser);
   }
 
-  // Connection de connection de facon anonyme
+  // Function de connection de facon anonyme
   Future<UserModel> signInAnonymously() async {
     final authResult = await _firebaseAuth.signInAnonymously();
    // print(_userFromFirebase(authResult.user).uid);
     return _userFromFirebase(authResult.user);
+  }
+
+  // Function de connection avec Google
+  @override
+  Future<UserModel> signInWithGoogle() async {
+    GoogleSignIn googleSignIn = GoogleSignIn();
+    GoogleSignInAccount googleUserAccount = await googleSignIn.signIn();
+    if(googleUserAccount != null){
+      GoogleSignInAuthentication googleAuth = await googleUserAccount.authentication;
+      if(googleAuth.accessToken != null && googleAuth.idToken != null){
+        final authResult = await _firebaseAuth.signInWithCredential(
+            GoogleAuthProvider.credential(
+                accessToken: googleAuth.accessToken,
+                idToken: googleAuth.idToken
+            )
+        );
+        print("Le nom d'affichage : ${authResult.user.displayName}");
+        return _userFromFirebase(authResult.user);
+      } else {
+        throw PlatformException(
+          code: "ERROR_MISSING_GOOGLE_AUTH_TOKEN",
+          message: "Misson Google Auth Token",
+        );
+      }
+    } else {
+      throw PlatformException(
+        code: "ERROR_ABORTED_BY_USER",
+        message: 'Sign in aborted by user',
+      );
+    }
   }
 
   // Fonction de deconnectio
