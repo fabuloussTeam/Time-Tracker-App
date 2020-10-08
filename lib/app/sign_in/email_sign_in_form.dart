922103f88fbe26c1b114e6cc55edce4e3ff924ce
fabuloussTeam <tjomemmanuel@gmail.com>
2020-10-08 18:05:38 +0100

    Mise en place d'un enum pour changer de formulaire

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -1,47 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:timetrackerapp/common_widgets/form_submit_button.dart';
 
- final TextEditingController _emailController = TextEditingController();
- final TextEditingController _passwordController = TextEditingController();
 
- void _submit(){
-   print("email: ${_emailController.text} password: ${_passwordController.text}");
- }
+enum EmailSignInFormType { signin, register }
 
+class EmailSignInForm extends StatefulWidget {
 
-
-List<Widget> _buildChildren(){
-  return [
-    TextField(
-      controller: _emailController,
-      decoration: InputDecoration(
-        labelText: 'Email',
-        hintText: 'test@gmail.com',
-      ),
-    ),
-    SizedBox(height: 8.0,),
-    TextField(
-      controller: _passwordController,
-      decoration: InputDecoration(
-        labelText: 'Password',
-      ),
-      obscureText: true,
-    ),
-    SizedBox(height: 8.0,),
-      FormSubmitButton(
-        onPressed: _submit,
-        text: "Sign in",
-    ),
-    SizedBox(height: 8.0,),
-    FlatButton(
-        onPressed: (){},
-        child: Text("Need an account? Register"),
-    )
-  ];
+  _EmailSignInFormState createState() => _EmailSignInFormState();
 }
 
 //mainAxisSize permet mettre la hauteur sur l'espace ocuuper nette
-class EmailSignInForm extends StatelessWidget {
+class _EmailSignInFormState extends State<EmailSignInForm> {
+
+  var _formType = EmailSignInFormType.signin;
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _submit(){
+    print("email: ${_emailController.text} password: ${_passwordController.text}");
+  }
+
+  List<Widget> _buildChildren(){
+    final primaryText = _formType == EmailSignInFormType.signin ? "Sign in" : "Create an account";
+    final secondaryText = _formType == EmailSignInFormType.signin ? "Need an account register" : "Have an account? Sign in";
+    print(secondaryText);
+    return [
+      TextField(
+        controller: _emailController,
+        decoration: InputDecoration(
+          labelText: 'Email',
+          hintText: 'test@gmail.com',
+        ),
+      ),
+      SizedBox(height: 8.0,),
+      TextField(
+        controller: _passwordController,
+        decoration: InputDecoration(
+          labelText: 'Password',
+        ),
+        obscureText: true,
+      ),
+      SizedBox(height: 8.0,),
+      FormSubmitButton(
+        onPressed: _submit,
+        text: primaryText,
+      ),
+      SizedBox(height: 8.0,),
+      FlatButton(
+        onPressed: (){},
+        child: Text(secondaryText),
+      )
+    ];
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Padding(
