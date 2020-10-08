799e400b87e942b0724396a46f4812f84fa163ae
fabuloussTeam <tjomemmanuel@gmail.com>
2020-10-08 11:16:22 +0100

    Ajout de la page email_sign_in_page

---
+++ lib/app/sign_in/email_sign_in_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class EmailSignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign In"),
+        centerTitle: true,
+      ),
+      body: _builContext(),
+    );
+  }
+
+  Widget _builContext() {
+    return Container();
+  }
+}
+
+
