d50e4d61d30bdb1e325979eb6b6a3426c19affd5
fabuloussTeam <tjomemmanuel@gmail.com>
2020-10-15 08:45:01 +0100

    Creation de la fonction updateWith dans email_sign_in_bloc pour mettre a jour le formulaire a partir de la fonction copyWith du model

--- lib/app/sign_in/email_sign_in_model.dart
+++ lib/app/sign_in/email_sign_in_model.dart
@@ -3,9 +3,19 @@ enum EmailSignInFormType { signin, register }
 class EmailSignInModel {
   final String email;
   final String password;
+  final EmailSignInModel formType;
   final bool isLoading;
   final bool submitted;
-  final EmailSignInModel formType;
 
   EmailSignInModel({this.email, this.password, this.isLoading, this.submitted, this.formType});
+
+   copyWith( {email, password, formType, isLoading, submitted} ) {
+    EmailSignInModel(
+      email: email,
+      password: password,
+      formType: formType,
+      isLoading: isLoading,
+      submitted: submitted
+    );
+  }
 }
