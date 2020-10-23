bef064a0c31b16e6a0374e46dcdad6fe603280d2
fabuloussTeam <tjomemmanuel@gmail.com>
2020-10-23 09:44:21 +0100

    Reecriture de la fonction creation createJob en fonction de _setData()

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -17,12 +17,15 @@ class FirestoreDatabase implements Database {
   final String uid;
   FirestoreDatabase({@required this.uid})  : assert(uid != null);
 
-  Future<void> createJob(Job job) async {
-   // final path = '/users/$uid/jobs/job_abc';
-    final path  = APIPath.job(uid, "job_abc");
-    final documentReference = FirebaseFirestore.instance.doc(path);
-    await documentReference.set(job.toMap());
+  Future<void> createJob(Job job) async => await _setData (
+      path: APIPath.job(uid, 'job_abc'),
+      data: job.toMap()
+  );
 
+  Future<void> _setData({String path, Map<String, dynamic> data}) async {
+    final reference = FirebaseFirestore.instance.doc(path);
+     print("$path: $data");
+    await reference.set(data);
   }
 
 }
