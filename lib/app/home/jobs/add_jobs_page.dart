af5ff76fab6ce9c1c2cf1de0f124340277093299
fabuloussTeam <tjomemmanuel@gmail.com>
2020-10-27 05:45:51 +0100

    Mise en place de la fonction Form() et sa globalKey

--- lib/app/home/jobs/add_jobs_page.dart
+++ lib/app/home/jobs/add_jobs_page.dart
@@ -1,27 +1,54 @@
 import 'package:flutter/material.dart';
 
 class AddJobPage extends StatefulWidget {
-  
+
   static Future<void> show(BuildContext context) async {
     await Navigator.of(context).push(
-      MaterialPageRoute(
-          builder: (context) => AddJobPage(),
-          fullscreenDialog: true
-      )
+        MaterialPageRoute(
+            builder: (context) => AddJobPage(),
+            fullscreenDialog: true
+        )
     );
   }
-  
+
   @override
   _AddJobPageState createState() => _AddJobPageState();
 }
 
 class _AddJobPageState extends State<AddJobPage> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  bool _validateAndSaveForm() {
+    final form = _formKey.currentState;
+    if(form.validate()){
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  void _submit(){
+    //TODO: Validate & save form
+    if(_validateAndSaveForm()) {
+      print("form saved ++++");
+      //TODO: submit data to Firestore
+
+    }
+ }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 2.0,
         title: Text("New Job"),
+        actions: [
+          FlatButton(
+            onPressed: _submit,
+            child: Text("Save", style: TextStyle(fontSize: 18, color: Colors.white),),
+          )
+        ],
       ),
       backgroundColor: Colors.grey[200],
       body: SingleChildScrollView(
@@ -29,8 +56,8 @@ class _AddJobPageState extends State<AddJobPage> {
           padding: EdgeInsets.all(16.0),
           child: Card(
             child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: _buildForm()
+                padding: EdgeInsets.all(16.0),
+                child: _buildForm()
             ),
           ),
         ),
@@ -40,6 +67,7 @@ class _AddJobPageState extends State<AddJobPage> {
 
   Widget _buildForm() {
     return Form(
+      key: _formKey,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: _buildFormChildren(),
@@ -55,8 +83,8 @@ class _AddJobPageState extends State<AddJobPage> {
       TextFormField(
         decoration: InputDecoration(labelText: "Rate per hour"),
         keyboardType: TextInputType.numberWithOptions(
-          signed: false,
-          decimal: false
+            signed: false,
+            decimal: false
         ),
       )
     ];
